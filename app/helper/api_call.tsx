dcec1db194c3b6fba5239bd4f2b263b08dd43f94
rjshah1902 <rjshah1902@gmail.com>
2026-03-30 16:16:21 +0530

    change url

--- app/helper/api_call.tsx
+++ app/helper/api_call.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://honeydew-armadillo-704093.hostingersite.com/api/page-visits/api",
+    baseURL: "https://honeydew-armadillo-704093.hostingersite.com/api",
     headers: {
         "Content-Type": "application/json",
     },
